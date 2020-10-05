df997d6aba7f4f55c763d98a7ceea23f50cfcb69
Princeyyyy <princemumo80@gmail.com>
2020-10-05 17:04:13 +0300

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserComponent} from './/user/user.component';
+import {RepoComponent} from './repo/repo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'user', component: UserComponent},
+  {path: 'repo', component: RepoComponent},
+  {path: '', redirectTo: '/user-list', pathMatch: 'full'},
 
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserServiceService } from '../user-service.service';
+import { User } from "../user";
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  newUser: User[];
+  particlesJS: any;
 
-  constructor() { }
+  constructor(private service: UserServiceService) {
+  }
 
-  ngOnInit(): void {
+  getSearchedUser(searchTerm) {
+    this.service.searchUser(searchTerm).then(
+      (success) => {
+        this.newUser = this.service.newUser;
+        console.log(this.newUser);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  ngOnInit() {
+    this.getSearchedUser('Princeyyyy');
+    // tslint:disable-next-line:prefer-const
+    let particlesJS;
+    particlesJS.load('particles-js', 'particles.json', null);
   }
 
 }
